ee058b1748de10b062ad3797a2452513cb0530ca
idan <ente4120@gmail.com>
2026-04-13 15:04:54 +0300

    feat: add staggered fade-in animation to user cards

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -4,8 +4,8 @@ import { UserCard } from './UserCard'
 export function UserList({ users }: { users: GitHubUser[] }) {
   return (
     <ul className="list-none p-0 m-0 flex flex-col gap-2">
-      {users.map((user) => (
-        <li key={user.id}>
+      {users.map((user, i) => (
+        <li key={user.id} className="animate-card-in" style={{ animationDelay: `${i * 30}ms` }}>
           <UserCard user={user} />
         </li>
       ))}
